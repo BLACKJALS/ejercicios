37b744f7b0de4202cefd65eaca998fc99cb17a26
Alejandro Leon <alejandro.leon@saitec.onmicrosoft.com>
2023-05-12 10:47:55 -0500

    se crea ejercicio árboles

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,29 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Ejercicios;
+
+Console.WriteLine("Ejercicio árboles");
+
+var arbol = new Arbol();
+var nodoArbol1 = arbol.InsertarNodo(4, null);
+arbol.ProcesarNodo(nodoArbol1);
+
+
+var arbol2 = new Arbol();
+var nodoArbol2 = arbol2.InsertarNodo(4, null);
+arbol2.InsertarNodo(2, nodoArbol2);
+arbol2.InsertarNodo(1, nodoArbol2);
+arbol2.ProcesarNodo(nodoArbol2);
+
+
+var arbol3 = new Arbol();
+var nodoArbol3 = arbol3.InsertarNodo(4, null);
+arbol3.InsertarNodo(1, nodoArbol3);
+arbol3.InsertarNodo(2, nodoArbol3);
+var n = arbol3.InsertarNodo(5, nodoArbol3);
+
+arbol3.InsertarNodo(3, nodoArbol3.Descendiente.Semejante);
+arbol3.InsertarNodo(1, n);
+arbol3.InsertarNodo(4, n);
+//arbol3.InsertarNodo(1, n);
+
+arbol3.ProcesarNodo(nodoArbol3);
---
+++ Ejercicios/ServicioArboles.cs
@@ -0,0 +1,71 @@
+﻿namespace Ejercicios {
+    public class Arbol {
+        public Nodo Raiz { get; set; }
+        public Nodo Creacion { get; set; }
+        public int i = 0;
+
+        public Arbol() {
+            Raiz = new Nodo(); 
+        }
+
+        public Nodo InsertarNodo(int peso, Nodo nodo) {
+            if (nodo == null) {
+                Raiz = new Nodo();
+                Raiz.Peso = peso;
+                Raiz.Descendiente = null;
+                Raiz.Semejante = null;
+                return Raiz;
+            }
+
+            if (nodo.Descendiente == null) {
+                var tmp = new Nodo();
+                tmp.Peso = peso;
+                nodo.Descendiente = tmp;
+                return tmp;
+            } else {
+                Creacion = nodo.Descendiente;
+                while (Creacion.Semejante != null) {
+                    Creacion = Creacion.Semejante;
+                }
+
+                var tmp = new Nodo();
+                tmp.Peso = peso;
+                Creacion.Semejante = tmp;
+                return tmp;
+            }
+        }
+
+        public void ProcesarNodo(Nodo nodo) {
+            if(nodo == null)
+                return;
+
+            for(int n = 0; n < i; n++)
+                Console.Write(" ");
+
+            Console.WriteLine(nodo.Peso);
+            if(nodo.Descendiente != null) {
+                i++;
+                ProcesarNodo(nodo.Descendiente);
+                i--;
+            }
+
+            if(nodo.Semejante != null) {
+                ProcesarNodo(nodo.Semejante);
+            }
+        }
+    }
+
+    public class Nodo {
+        public Nodo() {
+            Peso = 0;
+            Nivel = 0;
+            Descendiente = null;
+            Semejante = null;
+        }
+
+        public int Peso { get; set; }
+        public int Nivel { get; set; }
+        public Nodo Descendiente { get; set; }
+        public Nodo Semejante { get; set; }
+    }
+}
