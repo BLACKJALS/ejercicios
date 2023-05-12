cb1b8fc8ae42b393779614096e70a6ba896283c1
Alejandro Leon <alejandro.leon@saitec.onmicrosoft.com>
2023-05-12 07:35:44 -0500

    se realiza ejercicio de división de dos números

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,9 @@
 ﻿
+using Ejercicios;
+
 Console.WriteLine("Hello, World!");
+
+int numero1 = 12;
+int numero2 = 6;
+int resultado = ServicioOperacionNumeros.DividirNumero(numero1, numero2);
+Console.WriteLine($"El resultado de la división es: {resultado}");
---
+++ Ejercicios/ServicioOperacionNumeros.cs
@@ -0,0 +1,7 @@
+﻿namespace Ejercicios {
+    public static class ServicioOperacionNumeros {
+        public static int DividirNumero(int numero1, int numero2) {
+            return numero1 / numero2;
+        }
+    }
+}
