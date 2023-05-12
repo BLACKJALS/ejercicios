f31ed5c4110e01517b279c54066c55884405967b
Alejandro Leon <alejandro.leon@saitec.onmicrosoft.com>
2023-05-12 08:19:54 -0500

    se crea servicio para calcular representación según base

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,9 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Ejercicios;
+
+var numero = 287;
+var baseK = 8;
+var representacionSvc = new ServicioRepresentacionBase();
+var res = representacionSvc.GeneradorBase(numero, baseK);
+Console.WriteLine($"La representación en base k ({baseK}), del número {numero} es: {res}");
+
---
+++ Ejercicios/ServicioRepresentacionBase.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicios {
+    public class ServicioRepresentacionBase {
+        public List<int> Cocientes { get; set; }
+        public List<int> Residuos { get; set; }
+
+        public ServicioRepresentacionBase() {
+            Cocientes = new List<int>();
+            Residuos = new List<int>();
+        }
+
+        public string GeneradorBase(int cantidad, int baseK) {
+            Dividir(cantidad, baseK);
+            return GenerarBase();
+        }
+
+        private void Dividir(int numero, int baseK) {
+            if(numero < baseK)
+                return;
+
+            var valor = numero / baseK;
+            var res = numero % baseK;
+            Cocientes.Add(valor);
+            Residuos.Add(res);
+
+            if(res == 0)
+                return;
+
+            Dividir(valor, baseK);
+        }
+
+        private string GenerarBase() {
+            var ultimoCociente = Cocientes.LastOrDefault();
+            Residuos.Reverse();
+            return $"{ultimoCociente}{string.Join("", Residuos)}";
+        }
+    }
+}
