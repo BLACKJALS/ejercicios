1ae59c45ad2d239a478fceee4ed3d78daae880d9
Alejandro Leon <alejandro.leon@saitec.onmicrosoft.com>
2023-05-12 08:31:15 -0500

    cambiis mínimos

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -2,7 +2,7 @@
 using Ejercicios;
 
 var numero = 287;
-var baseK = 8;
+var baseK = 4;
 var representacionSvc = new ServicioRepresentacionBase();
 var res = representacionSvc.GeneradorBase(numero, baseK);
 Console.WriteLine($"La representación en base k ({baseK}), del número {numero} es: {res}");
